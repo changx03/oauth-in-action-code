9fa0301b9a9c3aba2ed275844dd4db0c04b22a7a
Luke Chang <luke.x.chang@gmail.com>
2019-02-22 14:16:24 +1300

    formated ch-5-ex-1

--- exercises/ch-5-ex-1/protectedResource.js
+++ exercises/ch-5-ex-1/protectedResource.js
@@ -1,80 +1,85 @@
-var express = require("express");
-var bodyParser = require('body-parser');
-var cons = require('consolidate');
-var nosql = require('nosql').load('database.nosql');
-var cors = require('cors');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cons = require('consolidate')
+const nosql = require('nosql')
+const cors = require('cors')
 
-var app = express();
+const db = nosql.load('database.nosql')
+const app = express()
 
-app.use(bodyParser.urlencoded({ extended: true })); // support form-encoded bodies (for bearer tokens)
+app.use(bodyParser.urlencoded({ extended: true })) // support form-encoded bodies (for bearer tokens)
 
-app.engine('html', cons.underscore);
-app.set('view engine', 'html');
-app.set('views', 'files/protectedResource');
-app.set('json spaces', 4);
+app.engine('html', cons.underscore)
+app.set('view engine', 'html')
+app.set('views', 'files/protectedResource')
+app.set('json spaces', 4)
 
-app.use('/', express.static('files/protectedResource'));
-app.use(cors());
+app.use('/', express.static('files/protectedResource'))
+app.use(cors())
 
 var resource = {
-	"name": "Protected Resource",
-	"description": "This data has been protected by OAuth 2.0"
-};
+  name: 'Protected Resource',
+  description: 'This data has been protected by OAuth 2.0'
+}
 
-var getAccessToken = function(req, res, next) {
-	// check the auth header first
-	var auth = req.headers['authorization'];
-	var inToken = null;
-	if (auth && auth.toLowerCase().indexOf('bearer') == 0) {
-		inToken = auth.slice('bearer '.length);
-	} else if (req.body && req.body.access_token) {
-		// not in the header, check in the form body
-		inToken = req.body.access_token;
-	} else if (req.query && req.query.access_token) {
-		inToken = req.query.access_token
-	}
-	
-	console.log('Incoming token: %s', inToken);
-	nosql.one(function(token) {
-		if (token.access_token == inToken) {
-			return token;	
-		}
-	}, function(err, token) {
-		if (token) {
-			console.log("We found a matching token: %s", inToken);
-		} else {
-			console.log('No matching token was found.');
-		}
-		req.access_token = token;
-		next();
-		return;
-	});
-};
+var getAccessToken = function (req, res, next) {
+  // check the auth header first
+  var auth = req.headers['authorization']
+  var inToken = null
+  if (auth && auth.toLowerCase().indexOf('bearer') == 0) {
+    inToken = auth.slice('bearer '.length)
+  } else if (req.body && req.body.access_token) {
+    // not in the header, check in the form body
+    inToken = req.body.access_token
+  } else if (req.query && req.query.access_token) {
+    inToken = req.query.access_token
+  }
+  console.log('Incoming token: %s', inToken)
 
-var requireAccessToken = function(req, res, next) {
-	if (req.access_token) {
-		next();
-	} else {
-		res.status(401).end();
-	}
-};
+  db.one().make(function (builder) {
+    builder.where('access_token', inToken)
+    builder.callback(function (err, result) {
+      if (err) {
+        console.error('%s: %s', err.name, err.message)
+        res.status(500).end()
+        return
+      }
+      if (result) {
+        console.log(
+          'We found a matching token: %s for client: %s with scope:',
+          result.access_token,
+          result.client_id,
+          result.scope.join(' ')
+        )
+        req.access_token = result
+      } else {
+        console.log('No matching token was found')
+      }
+      next()
+    })
+  })
+}
 
-app.options('/resource', cors());
+var requireAccessToken = function (req, res, next) {
+  if (req.access_token) {
+    next()
+  } else {
+    res.status(401).end()
+  }
+}
 
-app.post("/resource", cors(), getAccessToken, function(req, res){
+app.options('/resource', cors())
 
-	if (req.access_token) {
-		res.json(resource);
-	} else {
-		res.status(401).end();
-	}
-	
-});
+app.post('/resource', cors(), getAccessToken, requireAccessToken, function (
+  req,
+  res
+) {
+  res.json(resource)
+})
 
 var server = app.listen(9002, 'localhost', function () {
-  var host = server.address().address;
-  var port = server.address().port;
+  var host = server.address().address
+  var port = server.address().port
 
-  console.log('OAuth Resource Server is listening at http://%s:%s', host, port);
-});
- 
+  console.log('OAuth Resource Server is listening at http://%s:%s', host, port)
+})
