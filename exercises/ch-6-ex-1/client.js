36de433c6a61e8106f5427491dbbe50e7bded4e4
Luke Chang <luke.x.chang@gmail.com>
2019-02-25 09:33:43 +1300

    formated and updated ch-6ex-1

--- exercises/ch-6-ex-1/client.js
+++ exercises/ch-6-ex-1/client.js
@@ -1,19 +1,19 @@
-var express = require("express");
-var path = require("path");
+var express = require('express')
+var path = require('path')
 
-var app = express();
+var app = express()
 
-app.set('view engine', 'html');
-app.set('views', 'files/client');
+app.set('view engine', 'html')
+app.set('views', 'files/client')
 
-app.use('/', express.static('files/client'));
+app.use('/', express.static('files/client'))
 
 var server = app.listen(9000, 'localhost', function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('OAuth Client is listening at http://%s:%s', host, port);
-});
+  var host = server.address().address
+  var port = server.address().port
+  console.log('OAuth Client is listening at http://%s:%s', host, port)
+})
 
-app.get("/*", function(req, res){
-	res.sendFile(path.join(__dirname, "files/client/index.html"));
-});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'files/client/index.html'))
+})
