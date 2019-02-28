e8632d2ce68def2b177fe52a22753380bfd15695
Luke Chang <luke.x.chang@gmail.com>
2019-02-28 13:31:42 +1300

    formated ch-8-ex-2

--- exercises/ch-8-ex-2/clientImplicitGrant.js
+++ exercises/ch-8-ex-2/clientImplicitGrant.js
@@ -1,19 +1,20 @@
-var express = require("express");
-var path = require("path");
+var express = require('express')
+var path = require('path')
 
-var app = express();
+var app = express()
 
-app.set('view engine', 'html');
-app.set('views', 'files/clientImplicitGrant');
+app.set('view engine', 'html')
+app.set('views', 'files/clientImplicitGrant')
 
-app.use('/', express.static('files/clientImplicitGrant'));
+app.use('/', express.static('files/clientImplicitGrant'))
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'files/clientImplicitGrant/callback.html'))
+})
 
 var server = app.listen(9000, 'localhost', function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('OAuth Client is listening at http://%s:%s', host, port);
-});
-
-app.get("/*", function(req, res){
-	res.sendFile(path.join(__dirname, "files/clientImplicitGrant/callback.html"));
-});
+  var host = server.address().address
+  var port = server.address().port
+  console.log('ch-8-ex-2')
+  console.log('OAuth Client is listening at http://%s:%s', host, port)
+})
